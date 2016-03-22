f64fb0a6044adbe590a047067f366712fc5f7171
Gavin Cabbage <gavin.cabbage@capitalone.com>
2016-03-21 23:02:20 -0400

    spec file read, lines matched against basic but broken regex

--- main.go
+++ main.go
@@ -1,24 +1,67 @@
 package main
 
+// TODO
+// - remove wildcard from end of regex and fix that
+// - fix repetitive for loop in parseSpecFile
+
 import (
     "fmt"
     "os"
+    "bufio"
+    "regexp"
+    "io"
     "errors"
+    "strings"
 )
 
-func processArgs(args []string) error {
-    if len(args) != 2 {
-        return errors.New("incorrect number of arguments")
+const (
+    specRegex string = "/[[:alnum:]._-]+@[[:alnum:]._/-]+:[[:alnum:]._/-]+|[[:alnum:]._/-]+:[[:alnum:]._/-]+|[[:alnum:]._/-]+|.*/g"
+)
+
+func parseSpecFile(reader *bufio.Reader) ([]string, error) {
+    lines := []string{}
+    for line, eof := reader.ReadString('\n'); eof != io.EOF; line, eof = reader.ReadString('\n') {
+        if eof != nil {
+            return lines, errors.New("problem reading spec file")
+        }
+        line = strings.TrimSpace(line)
+        fmt.Println(line)
+        if match, err := regexp.MatchString(specRegex, line); err != nil {
+            return lines, err
+        } else if match {
+            lines = append(lines, line)
+        } else {
+            return lines, errors.New("spec files syntax incorrect")
+        }
     }
-    return nil
+    return lines, nil
 }
 
 func main() {
 
-    if e := processArgs(os.Args); e != nil {
-        panic(e)
+    numArgs := len(os.Args)
+    if numArgs < 0 || numArgs > 2 {
+        panic("incorrect number of arguments")
     }
-
     fmt.Println(os.Args)
+
+    specFile := os.Stdin
+    if numArgs == 2 {
+        var err error
+        specFile, err = os.Open(os.Args[1])
+        if err != nil {
+            panic("could not open spec file")
+        }
+    }
+    reader := bufio.NewReader(specFile)
+
+    lines, err := parseSpecFile(reader)
+    if err != nil {
+        panic(err)
+    }
+    fmt.Println(lines)
+
+    // TODO
+    // - now we have the spec file, time to tail -f them all...
 
 }
--- main_test.go
+++ main_test.go
@@ -2,14 +2,14 @@ package main
 
 import "testing"
 
-func Test_processArgsReturnsErrorOnSingleArgument(t *testing.T) {
+func Test_checkArgs_ReturnsErrorOnSingleArgument(t *testing.T) {
     mockArgs := []string{"first"}
     if processArgs(mockArgs) == nil {
         t.Fail()
     }
 }
 
-func Test_processArgsReturnsNilOnTwoArguments(t *testing.T) {
+func Test_checkArgs_ReturnsNilOnTwoArguments(t *testing.T) {
     mockArgs := []string{"first", "second"}
     if processArgs(mockArgs) != nil {
         t.Fail()
